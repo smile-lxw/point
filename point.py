d3ea307b0ab428839f34983a3bd97fb21b332d1f
smile-lxw <120617108+smile-lxw@users.noreply.github.com>
2023-04-27 19:30:43 +0800

    Update point.py

--- point.py
+++ point.py
@@ -61,7 +61,7 @@ def point():
             st.write(fix)
             # 上传文件
             # 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
-            auth = oss2.Auth('LTAI5tKD3Cb6DmRX7CYLbDXk', 'gxqnUmmmbTlvGncIsYWin1LO3rUoM2')
+            auth = oss2.Auth('LTAI5tKD3Cb6DmRX7', 'gxqnUmmmbTlvGncIsYW')
             # yourEndpoint填写Bucket所在地域对应的Endpoint。以华东1（杭州）为例，Endpoint填写为https://oss-cn-hangzhou.aliyuncs.com。
             # 填写Bucket名称。
             bucket = oss2.Bucket(auth, 'oss-cn-beijing.aliyuncs.com', 'dataface')
